c57492bf323b030192eb54867fae338c2436ea19
ajayanilsree <ajayanilsree@gmail.com>
2025-12-29 01:37:50 +0530

    uploaded

--- mydjango/djangoA/djangoB/views.py
+++ mydjango/djangoA/djangoB/views.py
@@ -4,4 +4,46 @@ from django.http import HttpResponse
 # Create your views here.
 
 def print_hello(request):
-    return HttpResponse("hello django i m from djangoB")
+    movie_details = {
+        'movies': 
+        [
+            {
+                'title': 'The Godfather',
+                'year': 1972,
+                'summary': '',
+                'success': True,
+            },
+            {
+                'title': 'The Dark Knight',
+                'year': 2008,
+                'summary': 'Batman faces the Joker in Gotham City',
+                'success': False,
+            },
+            {
+                'title': 'Inception',
+                'year': 2010,
+                # 'summary': 'A thief steals secrets through dream invasion',
+                'success': True,
+            },
+            {
+                'title': 'Interstellar',
+                'year': 2014,
+                'summary': 'Explorers travel through a wormhole in space',
+                'success': True,
+            },
+            {
+                'title': 'Parasite',
+                'year': 2019,
+                'summary': 'A poor family infiltrates a wealthy household',
+                'success': True,
+            },
+            {
+                'title': 'Gladiator',
+                'year': 2000,
+                'summary': 'A Roman general seeks revenge',
+                'success': False,
+            }
+        ]
+    }
+
+    return render(request, 'index.html', movie_details)
