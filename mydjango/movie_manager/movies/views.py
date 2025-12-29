c38d9d532734e859bcfe1340317b948eb9fcb7cd
ajayanilsree <ajayanilsree@gmail.com>
2025-12-29 23:19:11 +0530

    uploaded

--- mydjango/movie_manager/movies/views.py
+++ mydjango/movie_manager/movies/views.py
@@ -1,10 +1,47 @@
 from django.shortcuts import render
 
 def create(request):
+    if request.POST:
+        print(request.POST.get('title'))
+        print(request.POST.get('year'))
+        print(request.POST.get('summary'))
     return render(request,'create.html')
 
 def list(request):
-    return render(request,'list.html')
+    movie_details = {
+        'movies': 
+        [
+            {
+                'title': 'The Godfather',
+                'year': 1972,
+                'summary': 'Based on Mario Puzo',
+                'success': True,
+                'img':'godfather.jpg',
+            },
+            {
+                'title': 'The Dark Knight',
+                'year': 2008,
+                'summary': 'Batman faces the Joker in Gotham City',
+                'success': False,
+                'img':'darkknight.jpg',
+            },
+            {
+                'title': 'Inception',
+                'year': 2010,
+                'summary': 'A thief steals secrets through dream invasion',
+                'success': True,
+                'img':'inception.jpg',
+            },
+            {
+                'title': 'Interstellar',
+                'year': 2014,
+                'summary': 'Explorers travel through a wormhole in space',
+                'success': True,
+                'img':'interstellar.jpg',
+            }
+        ]
+    }
+    return render(request,'list.html',movie_details)
 
 def edit(request):
     return render(request,'edit.html')
