11ef0459b61af7f2937cc47c903d50dba07e4327
ajayanilsree <ajayanilsree@gmail.com>
2025-12-30 16:02:58 +0530

    uploaded

--- mydjango/movie_manager/movies/views.py
+++ mydjango/movie_manager/movies/views.py
@@ -1,50 +1,53 @@
 from django.shortcuts import render
-
+from . models import MovieInfo
 def create(request):
     if request.POST:
-        print(request.POST.get('title'))
-        print(request.POST.get('year'))
-        print(request.POST.get('summary'))
+        title=request.POST.get('title')
+        year=request.POST.get('year')
+        summary=request.POST.get('summary')
+        movieinfo_obj=MovieInfo(title=title,year=year,summary=summary)
+        movieinfo_obj.save()
     return render(request,'create.html')
 
 def list(request):
-    movie_details = {
-        'movies': 
-        [
-            {
-                'title': 'The Godfather',
-                'year': 1972,
-                'summary': 'Based on Mario Puzo',
-                'success': True,
-                'img':'godfather.jpg',
-            },
-            {
-                'title': 'The Dark Knight',
-                'year': 2008,
-                'summary': 'Batman faces the Joker in Gotham City',
-                'success': False,
-                'img':'darkknight.jpg',
-            },
-            {
-                'title': 'Inception',
-                'year': 2010,
-                'summary': 'A thief steals secrets through dream invasion',
-                'success': True,
-                'img':'inception.jpg',
-            },
-            {
-                'title': 'Interstellar',
-                'year': 2014,
-                'summary': 'Explorers travel through a wormhole in space',
-                'success': True,
-                'img':'interstellar.jpg',
-            }
-        ]
-    }
-    return render(request,'list.html',movie_details)
+    # movie_details = {
+    #     'movies': 
+    #     [
+    #         {
+    #             'title': 'The Godfather',
+    #             'year': 1972,
+    #             'summary': 'Based on Mario Puzo',
+    #             'success': True,
+    #             'img':'godfather.jpg',
+    #         },
+    #         {
+    #             'title': 'The Dark Knight',
+    #             'year': 2008,
+    #             'summary': 'Batman faces the Joker in Gotham City',
+    #             'success': False,
+    #             'img':'darkknight.jpg',
+    #         },
+    #         {
+    #             'title': 'Inception',
+    #             'year': 2010,
+    #             'summary': 'A thief steals secrets through dream invasion',
+    #             'success': True,
+    #             'img':'inception.jpg',
+    #         },
+    #         {
+    #             'title': 'Interstellar',
+    #             'year': 2014,
+    #             'summary': 'Explorers travel through a wormhole in space',
+    #             'success': True,
+    #             'img':'interstellar.jpg',
+    #         }
+    #     ]
+    # }
+    movie_details=MovieInfo.objects.all()
+    return render(request,'list.html',{'movies':movie_details})
 
 def edit(request):
-    return render(request,'edit.html')
+    return render(request,'edit.html')  
 
 def index(request):
     return render(request,'index.html')
